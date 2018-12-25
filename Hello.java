797de62280392bac0228625154ccae925dbb1f79
Khouja Boubaker <khouja.boubaker@yahoo.com>
2018-12-25 12:20:17 +0100

    test job

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@ public class Hello {
 	
 	public static void main (String[]  args){
 		
-		for (int i=1;i<=10;i++){
-			System.out.println("Hello world ..."+i);
+		for (int i=1;i<=5;i++){
+			System.out.println("Khouja boubaker..."+i);
 		}
 	}
 }
